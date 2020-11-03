2d54e81cf4de894ad4cbf4565254ed5ca1528563
Aman Chhetri <gattiflab@gmail.com>
2020-11-03 15:05:21 +0530

    Lead Form

--- src/components/LeadForm/LeadForm.js
+++ src/components/LeadForm/LeadForm.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import './LeadForm.css';
 
 class LeadForm extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      contact: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      name: this.state.name,
+      email: this.state.email,
+      contact: this.state.contact
+    }
+    console.log(userData)
+  }
 
 
   render() { 
+    const {name, email, contact} = this.state;
     return (
       <div>
-        Lead form
+        <p>Sign Up so that we can provide you the best information.</p>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" value={name} onChange={this.handleChange}/>
+          </div>
+          <div>
+            <label for="email">E-Mail Id</label>  
+            <input type="email" name="email" value={email} onChange={this.handleChange}/>
+          </div>
+          <div>
+            <label for="contact">Contact No.</label>
+            <input type="number" name="contact" value={contact} onChange={this.handleChange}/>
+          </div>
+          <div className="submit-button">
+            <input type="submit" value="Submit" />
+          </div>
+        </form> 
       </div>
     );
   }
