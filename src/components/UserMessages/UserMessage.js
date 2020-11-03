4349479413e43381239bab9c24aeb9bc74ae2e5f
Abhishek Sharma <abhishek0058@gmail.com>
2020-11-03 13:59:26 +0530

    removed redundant code

--- src/components/UserMessages/UserMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-//import './UserMessage.css'
-
-function UserMessage(props) {
-    const {message} = props;
-    return <div className="user-chat">{message}</div>
-}
-
-export default UserMessage
